8bcd49377121c18a0fea33a5222d1493a2bb8157
Amirkhan <amirhan9878@gmail.com>
2021-06-14 23:07:28 +0600

    Create project tree structure by maven         Add dependency Picocli         Add Annotation Processor (for Picocli)

---
+++ quizer-cli/src/main/java/com/quizer/Main.java
@@ -0,0 +1,14 @@
+package com.quizer;
+
+/**
+ * Main class
+ */
+public class Main {
+    /**
+     * Entry point for project
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Quizer");
+    }
+}
---
+++ quizer-cli/src/test/java/com/quizer/MainTest.java
@@ -0,0 +1,18 @@
+package com.quizer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple app.
+ */
+class MainTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void test() {
+        assertEquals(1, 1);
+    }
+}
